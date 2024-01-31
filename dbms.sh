803a78b6963d73d75eced2db5a65057fb82ebb4d
Blitz576 <anagy8050@gmail.com>
2024-01-31 17:27:52 +0200

    add create DataBAse and create databases main directory

--- dbms.sh
+++ dbms.sh
@@ -1,64 +1,78 @@
 #!/bin/bash
-#create a database main menu
+#DataBase Main System
 
-PS3="your choice : "
+# change the select prompt
+PS3="Main System > "
 export LC_COLLATE=C
 shopt -s extglob
 
-#change the select prompt
-PS3="Your choice: "
+# main functions
 
+function create_main_DataBase_directory () {
+    echo "Welcome to our Bash Sql system."
+    echo "After reading this line, a database directory has been created in your current directory under the name of 'DataBases'."
+    echo "If you have used our program before, don't worry ..."
+    
+    if [ ! -d "DataBases" ]; then
+        mkdir "DataBases"
+    fi
+}
 
-    select menu in "Create a Database" "List Databases" "Connect To Database" "Drop Database" "Exit"; do
-        case $REPLY in
-            1)
-                echo "Enter the database name: "
-                read dbName
-
-                while [ -d database/$dbName ]; do
-                    echo "$dbName Already Exists. Please Enter Another Name: "
-                    read dbName
-                done
-
-                case $dbName in
-                    [0-9]*)
-                        echo "Database Name Cannot Start With Numbers."
-                        ;;
-                    ' '|*' '*|' '*|*' ')
-                    echo "Database Name Should Not have any Spaces"
-                    ;;
-                    
-                    +([a-zA-Z]))
-                        mkdir -p database/$dbName
-                        echo "$dbName Created Successfully."
-                        ;;
-		            *)
-                        echo "Database Name Cannot Have Special Characters."
-                        ;;
-
-                esac
-                ;;
-            2)
-                if [ -n "$(ls database/)" ]; then
-                echo Available Databases:
-                ls -F database/ | grep /
-          	
-                else
-	            echo "There Are No Databases"
-                fi
-                ;;
-            3)
-                echo "Option 3 is chosen"
-                ;;
-            4)
-                echo "Option 4 is chosen"
-                ;;
-            5)
-                echo "System ended."
-                break;
-                ;;
-            *)
-                echo "Invalid option. Please choose a number between 1 and 5."
-                ;;
-        esac
+function createDataBase () {
+    local local_dbName="$1"
+    while [ -d "database/$local_dbName" ]; do
+        echo "$local_dbName Already Exists. Please Enter Another Name: "
+        read local_dbName
     done
+
+    case $local_dbName in
+        [0-9]*)
+            echo "Database Name Cannot Start With Numbers."
+            ;;
+        *" "*)
+            echo "Database Name Should Not Have Any Spaces."
+            ;;
+        *[!a-zA-Z]*)
+            echo "Database Name Cannot Have Special Characters."
+            ;;
+        *)
+            mkdir -p "database/$local_dbName"
+            echo "$local_dbName Created Successfully."
+            ;;
+    esac
+}
+
+# create a directory for user databases
+create_main_DataBase_directory
+
+
+select menu in "Create a Database" "List Databases" "Connect To Database" "Drop Database" "Exit"; do
+    case $REPLY in
+        1)
+            echo "Enter the database name: "
+            read dbName
+            createDataBase "$dbName"
+            ;;
+        2)
+            if [ -n "$(ls -d database/*/)" ]; then
+                echo "Available Databases:"
+                ls -d database/*/ | sed 's|.*/||'
+            else
+                echo "There Are No Databases"
+            fi
+            ;;
+        3)
+            echo "Option 3 is chosen"
+            ;;
+        4)
+            echo "Option 4 is chosen"
+            ;;
+        5)
+            echo "System ended."
+            break
+            ;;
+        *)
+            echo "Invalid option. Please choose a number between 1 and 5."
+            ;;
+    esac
+done
